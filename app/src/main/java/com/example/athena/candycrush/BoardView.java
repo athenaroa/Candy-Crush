596c87d0d9b7ecbfed31da4afa9764c728486a58
Akroa <akroa@uci.edu>
2018-05-16 18:56:24 -0700

    Reimplementation with the same format but adding 2D arrays

--- app/src/main/java/com/example/athena/candycrush/BoardView.java
+++ app/src/main/java/com/example/athena/candycrush/BoardView.java
@@ -14,38 +14,58 @@ import android.view.SurfaceView;
 import java.util.Random;
 
 public class BoardView extends SurfaceView implements SurfaceHolder.Callback {
-    Bitmap redCandy =BitmapFactory.decodeResource(getResources(),R.drawable.redcandy); //Put an image called 'image' under the folder drawable
-    Bitmap greenCandy =BitmapFactory.decodeResource(getResources(),R.drawable.greencandy);
-    Bitmap blueCandy =BitmapFactory.decodeResource(getResources(),R.drawable.bluecandy);
-
-    Bitmap redJelly  =BitmapFactory.decodeResource(getResources(),R.drawable.redjelly);
-    Bitmap greenJelly  =BitmapFactory.decodeResource(getResources(),R.drawable.greenjelly);
-    Bitmap blueJelly  =BitmapFactory.decodeResource(getResources(),R.drawable.bluejelly);
-    Bitmap pinkJelly  =BitmapFactory.decodeResource(getResources(),R.drawable.pinkjelly);
-
-    Bitmap candyCorn  =BitmapFactory.decodeResource(getResources(),R.drawable.candycorn);
-    Bitmap candyCornOpen  =BitmapFactory.decodeResource(getResources(),R.drawable.candycornopen);
-
-
-
 
     float x;
     float y;
+    int score = 0;
+    private Bitmap[] candy;
+    private int[][] board;
+    private int numRow = 9;
+    private int numColumn = 9;
+    Bitmap redCandy;
+    Bitmap greenCandy;
+    Bitmap blueCandy;
+    Bitmap redJelly;
+    Bitmap greenJelly;
+    Bitmap blueJelly;
+    Bitmap pinkJelly;
+    Bitmap candyCorn;
+    Bitmap candyCornOpen;
 
-    public BoardView (Context context)
-    {
+
+    public BoardView(Context context) {
         super(context);
-        //Notify the SurfaceHolder that you'd like to receive SurfaceHolder callbacks.
         getHolder().addCallback(this);
         setFocusable(true);
+
         //Initialize game state variables and the game board variables
 
+        board = new int[9][9];
 
+
+        redCandy = BitmapFactory.decodeResource(getResources(), R.drawable.redcandy); //Put an image called 'image' under the folder drawable
+        greenCandy = BitmapFactory.decodeResource(getResources(), R.drawable.greencandy);
+        blueCandy = BitmapFactory.decodeResource(getResources(), R.drawable.bluecandy);
+
+        redJelly = BitmapFactory.decodeResource(getResources(), R.drawable.redjelly);
+        greenJelly = BitmapFactory.decodeResource(getResources(), R.drawable.greenjelly);
+        blueJelly = BitmapFactory.decodeResource(getResources(), R.drawable.bluejelly);
+        pinkJelly = BitmapFactory.decodeResource(getResources(), R.drawable.pinkjelly);
+
+        candyCorn = BitmapFactory.decodeResource(getResources(), R.drawable.candycorn);
+        candyCornOpen = BitmapFactory.decodeResource(getResources(), R.drawable.candycornopen);
+
+        for (int i = 0; i < numRow; i++) {
+            for (int j = 0; j < numColumn; j++) {
+                Random random = new Random();
+                int c = random.nextInt(9);
+                board[i][j] = c;
+            }
+        }
     }
 
     @Override
-    public void surfaceCreated (SurfaceHolder holder)
-    {
+    public void surfaceCreated(SurfaceHolder holder) {
         //Construct game initial state
         //Create the Candy Board
         //Initialize the board with random candies
@@ -57,8 +77,7 @@ public class BoardView extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height)
-    {
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
         //Called in response to changes in the surface
         //Write code here to do the necessary tasks such as adding new random candies from
         //the top of the board when the dimensions of the board change on eliminating the matched candies
@@ -66,16 +85,14 @@ public class BoardView extends SurfaceView implements SurfaceHolder.Callback {
     }
 
     @Override
-    public void surfaceDestroyed(SurfaceHolder holder)
-    {
+    public void surfaceDestroyed(SurfaceHolder holder) {
         //This is called immediately before a surface is being destroyed.
         //Write code here that needs to be executed just before the surface is destroyed
     }
 
 
     @Override
-    public boolean onTouchEvent(MotionEvent e)
-    {
+    public boolean onTouchEvent(MotionEvent e) {
         //Update game state in response to events:
         //touch-down, touch-up, and touch-move
         //Perform operations to check if the touch event is a valid move
@@ -83,87 +100,103 @@ public class BoardView extends SurfaceView implements SurfaceHolder.Callback {
         //and moving the candies above the eliminated row/column to their appropriate positions.
         x = e.getX();
         y = e.getY();
+        
 
-
-        switch(e.getAction())
-        {
-            case (MotionEvent.ACTION_MOVE) :
+        switch (e.getAction()) {
+            case (MotionEvent.ACTION_MOVE):
+                /*
                 Canvas c = getHolder().lockCanvas();
                 Paint paint = new Paint();
                 paint.setColor(Color.BLUE);
-                c.drawRect(x,y,x+100,y+100,paint);
+                c.drawRect(x,y,x+20,y+20,paint);
                 getHolder().unlockCanvasAndPost(c);
+                */
                 return true;
+
             case (MotionEvent.ACTION_UP):
+
+                /*
                 Canvas d = getHolder().lockCanvas();
                 Paint paint2 = new Paint();
                 paint2.setColor(Color.BLACK);
-                d.drawRect(x,y,x+100,y+100,paint2);
+                d.drawRect(x,y,x+20,y+20,paint2);
                 getHolder().unlockCanvasAndPost(d);
+                */
+
+
                 return true;
-             case (MotionEvent.ACTION_DOWN):
+            case (MotionEvent.ACTION_DOWN):
+                /*
                  Canvas f = getHolder().lockCanvas();
                  Paint paint3 = new Paint();
                  paint3.setColor(Color.GREEN);
-                 f.drawRect(x,y,x+100,y+100,paint3);
+                 f.drawRect(x,y,x+20,y+20,paint3);
                  getHolder().unlockCanvasAndPost(f);
-                 return true;
+                */
+
+
+                return true;
             default:
                 return super.onTouchEvent(e);
-
-
         }
 
     }
 
     @Override
-    protected void onDraw(Canvas c)
-    {
+    protected void onDraw(Canvas c) {
         super.onDraw(c);
         c.drawColor(Color.YELLOW); //Set the background to yellow
 
-        for(int i = 100; i < 1000; i += 100) //Width
-        {
-            for(int j = 500; j < 1400; j += 100) //Height
-            {
-                Rect dst=new Rect();
-                dst.set(i,j ,i + 90,j + 90);
+        Paint s = new Paint();
+        s.setColor(Color.BLACK);
+        s.setTextSize(80);
+        c.drawText("Score = " + score, 100, 300, s);
+        Rect dst = new Rect();
 
-                Random random = new Random();
-                int candy = random.nextInt(9);
-                switch(candy)
-                {
-                    case(0):
-                        c.drawBitmap(redCandy, null,dst,null);
+        for (int i = 0; i < numRow; i++) {
+            for (int j = 0; j < numColumn; j++) {
+                int index = board[i][j];
+
+                dst.set(j * 120, (i * 120) + 450, (j + 1) * 120, ((i + 1) * 120) + 450);
+                switch (index) {
+                    case (0):
+                        c.drawBitmap(redCandy, null, dst, null);
                         break;
-                    case(1):
-                        c.drawBitmap(blueCandy, null,dst,null);
+                    case (1):
+                        c.drawBitmap(blueCandy, null, dst, null);
+
                         break;
-                    case(2):
-                        c.drawBitmap(greenCandy, null,dst,null);
+                    case (2):
+                        c.drawBitmap(greenCandy, null, dst, null);
+
                         break;
-                    case(3):
-                        c.drawBitmap(redJelly, null,dst,null);
+                    case (3):
+                        c.drawBitmap(redJelly, null, dst, null);
+
                         break;
-                    case(4):
-                        c.drawBitmap(blueJelly, null,dst,null);
+                    case (4):
+                        c.drawBitmap(blueJelly, null, dst, null);
                         break;
-                    case(5):
-                        c.drawBitmap(greenJelly, null,dst,null);
+                    case (5):
+                        c.drawBitmap(greenJelly, null, dst, null);
+
                         break;
-                    case(6):
-                        c.drawBitmap(pinkJelly, null,dst,null);
+                    case (6):
+                        c.drawBitmap(pinkJelly, null, dst, null);
+
                         break;
-                    case(7):
-                        c.drawBitmap(candyCorn, null,dst,null);
-                    case(8):
-                        c.drawBitmap(candyCornOpen, null,dst,null);
+                    case (7):
+                        c.drawBitmap(candyCorn, null, dst, null);
+
+                        break;
+                    case (8):
+                        c.drawBitmap(candyCornOpen, null, dst, null);
+
+                        break;
                 }
 
             }
         }
-        
+
     }
-
-
 }
